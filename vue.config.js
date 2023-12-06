040cb17b6948bedbd9f66325e9c0f62a4571443c
sehyeon An <sehyeon014@gmail.com>
2023-12-06 14:32:45 +0900

    fix: webSocket service proxy config

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     devtool: false,
   },
   devServer: {
-    client: {
-      webSocketURL: {
-        hostname: "localhost",
-        pathname: "/ws",
-        port: 8080,
-        protocol: "ws",
+    proxy: {
+      "/ws": {
+        target: "http://sc-backend-service:8080", // 백엔드 서비스의 클러스터 내부 DNS 이름과 포트
+        ws: true,
+        changeOrigin: true,
       },
     },
   },
