ddf41e2453ac4dfd5afd9b8aec200b69f6dd425a
You Jeong Jang <sunny819819@naver.com>
2023-12-11 16:38:02 +0900

    🔎웹소켓 에러 핸들링 코드 추가 / 포트 번호 삭제

--- src/components/CheckLogin.vue
+++ src/components/CheckLogin.vue
@@ -27,6 +27,7 @@ async function loginCheck() {
   }
 }
 
+/*
 // socket 서버에 연결
 function socketConnect() {
   const s_client = io('http://sc-chatting.com:5000',{
@@ -34,11 +35,42 @@ function socketConnect() {
     //transports: ['websocket']
   }).connect();
   
-  /*
-  const s_client = io(`http://sc-chatting.com:5000`).connect();
-  */
+  
+  //const s_client = io(`http://sc-chatting.com:5000`).connect();
+  
   userData.socket = s_client;
 }
+*/
+
+// socket 서버에 연결
+function socketConnect() {
+  const s_client = io('http://sc-chatting.com', {
+    path: '/socket.io', // 서버 path와 일치시켜준다
+    // transports: ['websocket']
+  });
+
+  // 연결 성공 시 이벤트 핸들러
+  s_client.on('connect', () => {
+    console.log('웹소켓 서버에 연결되었습니다.');
+    // 연결된 소켓을 userData.socket에 저장
+    userData.socket = s_client;
+  });
+
+  // 연결 실패 시 이벤트 핸들러
+  s_client.on('connect_error', (error) => {
+    console.error('웹소켓 연결 실패:', error);
+  });
+
+  // 연결이 끊긴 경우 이벤트 핸들러
+  s_client.on('disconnect', (reason) => {
+    console.warn('웹소켓 연결이 끊어졌습니다. 이유:', reason);
+  });
+
+  // 오류 발생 시 이벤트 핸들러
+  s_client.on('error', (error) => {
+    console.error('웹소켓 오류:', error);
+  });
+
 
 onMounted(async () => {
   const is_login = await loginCheck();
--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
       webSocketURL: {
         hostname: "sc-chatting.com",
         pathname: "/ws",
-        port: 80,
+        port: 5000,
         protocol: "ws",
       },
     },
